55060296fd55afbec23f1dd195823c7cfcc00410
emanoelcampos <emanoel.cv@gmail.com>
2023-05-01 08:15:29 -0300

    teste de responsividade no celular

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
- window.addEventListener("scroll", function() {
-        var footer = document.querySelector("footer");
-        var windowHeight = window.innerHeight;
-        var bodyHeight = document.body.offsetHeight;
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        var scrollBottom = bodyHeight - (scrollTop + windowHeight);
-        if (scrollBottom < 50) {
-            footer.style.display = "block";
-        } else {
-            footer.style.display = "none";
-        }
-    });
 
